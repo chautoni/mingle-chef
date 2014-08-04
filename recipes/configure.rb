c556bd5acd033ad29e7bd9a4a4c2f3dde7891b3e
Thach Chau <rog.kane@gmail.com>
2014-08-04 21:12:28 +0700

    Credentials file now stored on shared/config

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -3,7 +3,7 @@ include_recipe 'aws'
 node[:deploy].each do |app_name, deploy_config|
   aws_access_key_id = deploy_config[:application_variables][:aws_access_key_id]
   aws_secret_access_key = deploy_config[:application_variables][:aws_secret_access_key]
-  app_config_path = "#{deploy_config[:deploy_to]}/current/config"
+  app_config_path = "#{deploy_config[:deploy_to]}/shared/config"
 
   ['apple_push_notification.pem', 'application.yml', 'redis.yml', 'pusher.yml'].each do |file_name|
     aws_s3_file "#{app_config_path}/#{file_name}" do
