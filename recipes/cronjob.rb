8d838e49634cd51c44cca64e6ca8dd84b215956f
Thach Chau <rog.kane@gmail.com>
2014-08-07 15:22:12 +0700

    Update cronjob

--- recipes/cronjob.rb
+++ recipes/cronjob.rb
@@ -2,12 +2,17 @@ cron 'daily report' do
   hour '0'
   minute '30'
   day '*'
-  command 'cd /srv/mingle/current && RAILS_ENV=production bundle exec thor report_task:daily_system_report'
+  command 'cd /srv/www/mingle/current && RAILS_ENV=production bundle exec thor report_task:daily_system_report'
 end
 
 cron 'weekly unread messages notification' do
   hour '0'
   minute '30'
   weekday '1'
-  command 'cd /srv/mingle/current && RAILS_ENV=production bundle exec rake users:send_unread_messages_reminder'
+  command 'cd /srv/www/mingle/current && RAILS_ENV=production bundle exec rake users:send_unread_messages_reminder'
+end
+
+cron 'checking background job status' do
+  minute '10'
+  command "cd /srv/www/mingle/current && bundle exec rails runner -e production 'SystemMonitor::BackgroundJobMonitor.execute'"
 end
