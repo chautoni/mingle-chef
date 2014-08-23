398a26e73ed80e601a07417addf5f9b8e8925e86
Thach Chau <rog.kane@gmail.com>
2014-08-23 16:08:36 +0700

    Merge papertrail recipe with setup recipe

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -10,7 +10,18 @@ node[:deploy].each do |app_name, deploy_config|
     recursive true
   end
 
-  ['apple_push_notification.pem', 'application.yml', 'redis.yml', 'pusher.yml', 'remote_syslog.yml'].each do |file_name|
+  aws_s3_file '/etc/rsyslog.conf' do
+    bucket node[:mingle][:s3_configuration_bucket]
+    remote_path 'rsyslog.conf'
+    aws_access_key_id aws_access_key_id
+    aws_secret_access_key aws_secret_access_key
+  end
+
+  execute 'restart rsyslog' do
+    command 'service rsyslog restart'
+  end
+
+  ['apple_push_notification.pem', 'application.yml', 'redis.yml', 'pusher.yml'].each do |file_name|
     aws_s3_file "#{app_config_path}/#{file_name}" do
       bucket node[:mingle][:s3_configuration_bucket]
       remote_path file_name
